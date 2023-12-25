6629c1f1253df16507d0b4a5b29b479768c1f957
xucz <xgfd3@users.noreply.github.com>
2023-12-25 10:17:29 +0800

    Update

--- china_resp_change.sh
+++ china_resp_change.sh
@@ -2,11 +2,12 @@
 git config --global user.email "sync2gitee@example.com"
 git config --global user.name "sync2gitee"
 
-cd ./cache/sync2gitee
+pwd
 git remote -v
 sed -ie 's/Yikun/xgfd3/g' README.md
 git status
 git add README.md
 git commit -m 'sync >> Update REAMDE'
 git push gitee
-cd -
+
+
